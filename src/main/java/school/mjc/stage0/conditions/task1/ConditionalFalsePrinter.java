c2a3fb0fca5e3f5db17967ab43227fb2b71f057e
Zotoff <paulzotoff@gmail.com>
2023-06-11 10:14:13 +0300

    First attempt

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int param = 10;
+        if (param < 5) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int param = 10;
+        if (param < 20) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
